07ea505b62471c46f9a42152de675fad29ad40fe
Selim <selimakin55@gmail.com>
2022-05-10 13:05:49 +0300

    Search results and pagination are added

---
+++ src/js/views/paginationView.js
@@ -0,0 +1,70 @@
+import icons from '../../img/icons.svg';
+import View from './View.js';
+
+class PaginationView extends View {
+  _parentElement = document.querySelector('.pagination');
+
+  addEventClick(handler) {
+    this._parentElement.addEventListener('click', function (e) {
+      const btn = e.target.closest('.btn--inline');
+      if (!btn) return;
+      const goToPage = +btn.dataset.goto;
+      handler(goToPage);
+    });
+  }
+
+  _generateMarkUp() {
+    const curPage = this._data.page;
+    const numPage = Math.ceil(
+      this._data.results.length / this._data.resultsPerPage
+    );
+
+    if (curPage === 1 && numPage > 1)
+      return `
+        <button data-goto="${
+          curPage + 1
+        }" class="btn--inline pagination__btn--next">
+          <span>Page ${curPage + 1}</span>
+          <svg class="search__icon">
+            <use href="${icons}#icon-arrow-right"></use>
+          </svg>
+        </button>
+      `;
+
+    if (curPage > 1 && numPage === curPage)
+      return `
+        <button data-goto="${
+          curPage - 1
+        }" class="btn--inline pagination__btn--prev">
+          <svg class="search__icon">
+            <use href="${icons}#icon-arrow-left"></use>
+          </svg>
+          <span>Page ${curPage - 1}</span>
+        </button>
+      `;
+
+    if (curPage > 1 && numPage > curPage)
+      return `
+        <button data-goto="${
+          curPage - 1
+        }" class="btn--inline pagination__btn--prev">
+          <svg class="search__icon">
+            <use href="${icons}#icon-arrow-left"></use>
+          </svg>
+          <span>Page ${curPage - 1}</span>
+        </button>
+        <button data-goto="${
+          curPage + 1
+        }" class="btn--inline pagination__btn--next">
+          <span>Page ${curPage + 1}</span>
+          <svg class="search__icon">
+            <use href="${icons}#icon-arrow-right"></use>
+          </svg>
+        </button>
+        `;
+
+    return '';
+  }
+}
+
+export default new PaginationView();
---
+++ src/js/views/resultsView.js
@@ -0,0 +1,29 @@
+import icons from '../../img/icons.svg';
+import View from './View.js';
+
+class ResultsView extends View {
+  _parentElement = document.querySelector('.results');
+  _errorMessage = 'No recipes found for your query! Please try again ;)';
+  _message = '';
+
+  _generateMarkUp() {
+    return `<li class="preview">
+    ${this._data.map(this._generateMarkUpView).join('')}
+  </li>
+    `;
+  }
+
+  _generateMarkUpView(meal) {
+    return `<a class="preview__link" href="#${meal.id}">
+    <figure class="preview__fig">
+      <img src="${meal.image}" alt="Test" />
+    </figure>
+    <div class="preview__data">
+      <h4 class="preview__title">${meal.title}</h4>
+      <p class="preview__publisher">${meal.publisher}</p>
+    </div>
+  </a>`;
+  }
+}
+
+export default new ResultsView();
